69563c2b3486ce360f1bbdd4f0b77b963fbaa81c
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-05-19 19:31:32 +0800

    creating the number tv1 given that channel is set to number 30 and set volume to 3, and i already print the ouput for TV1 to see if the code is working

--- TestTV.py
+++ TestTV.py
@@ -1,7 +1,21 @@
 #import TV from Tvclass.py file
 from tvClass import TV  
 
+#create Tv1's channel is 30 and volume level is 3
+tv1 = TV()
+tv1.setChannel(30)
+tv1.setVolume(3)
 
+
+
+
+
+
+
+
+
+#print the specific output for Tv1 and Tv2 that is given from the problem
+print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
 
 
 
