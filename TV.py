fc194511b0cd2f37781e64c925af9fe1dfbea028
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-05-19 17:08:35 +0800

    constructor

--- TV.py
+++ TV.py
@@ -1,5 +1,10 @@
 #Michael Angelo P. Biag
 #BSCOE 1-4
 
-#Create TV class
+# Create TV class
 class TV:
+# constructor
+    def __init__(self, channel = 1, volume = 1, switch = False):
+        self.channel = channel
+        self.volume = volume
+        self.switch = switch
