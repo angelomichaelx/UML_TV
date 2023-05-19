62c78cc61a9c3f3b87d4525f98053664c3a5c0e8
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-05-19 19:43:51 +0800

    adding some decoration for output

--- TestTV.py
+++ TestTV.py
@@ -13,9 +13,10 @@ tv2.setVolume(2)
 
 #print the specific output for Tv1 and Tv2 that is given from the problem
 #TV1
-print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
-
+print("\033[1;36m""=" * 45)
+print("\033[1;32m""tv1's channel is", tv1.getChannel(), "\033[1;36m""and " "\033[1;35m" "volume level is", tv1.getVolume())
+print("=" * 45)
 #TV2
-print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())
-
+print("\033[1;35m""tv2's channel is", tv2.getChannel(),"\033[1;36m" "and ""\033[1;32m" " volume level is", tv2.getVolume())
+print("=" * 45)
 
